1d70ff5f27b06f46b4e441bfb01dbf1eb63d3b91
Vitor <vmendesneto@gmail.com>
2022-04-20 22:32:45 -0300

    Changes.

---
+++ lib/src/controller/tir_controller.dart
@@ -0,0 +1,42 @@
+
+
+import 'dart:math';
+
+import 'package:flutter/cupertino.dart';
+
+class tirController {
+  static num irr(
+      {@required List<num>? values,
+        num guess = 0.1,
+        num tol = 1e-6,
+        num maxIter = 100}) {
+    num rn = guess;
+    num iterator = 0;
+    bool close = false;
+    while ((iterator < maxIter) && !close) {
+      final num rnp1 = rn - _npv_div_npvPrime(rn, values!);
+      final num diff = (rnp1 - rn).abs();
+      close = diff < tol;
+      iterator += 1;
+      rn = rnp1;
+    }
+
+    return rn;
+  }
+
+  static num npv({@required num? rate, @required List<num>? values}) {
+    return List<int>.generate(values!.length, (int index) => index)
+        .map((int index) => values[index] / pow(1 + rate!, index))
+        .fold(0, (num p, num c) => p + c);
+  }
+  static num _npv_div_npvPrime(num rate, List<num> values) {
+    final num t1 = npv(rate: rate, values: values);
+    final num t2 = _npvPrime(rate: rate, values: values);
+    return t1 / t2;
+  }
+  static num _npvPrime({@required num? rate, @required List<num>? values}) {
+    return List<int>.generate(values!.length, (int index) => index)
+        .map((int index) => -index * values[index] / pow(1 + rate!, index + 1))
+        .fold(0, (num p, num c) => p + c);
+  }
+}
---
+++ lib/src/variables.dart
@@ -0,0 +1,35 @@
+
+
+class Variables {
+  List<double> dataList = [];
+  List<double> jurosList = [];
+  List<String> dateList = [];
+  List<num> parcList = [];
+  List<num> amorList = [];
+  List<num> tirList = [];
+
+  double result = 0.00;
+  double? dado;
+  double saldodevedor = 0;
+  double? juros;
+  double vtir = 0.00;
+  num amortiza = 0;
+  num? periodo;
+  num taxa = 0;
+  int total = 0;
+  int nparc = 0;
+  int valor = 0;
+  DateTime? newDate;
+  double parcela = 0;
+  num carencia = 0;
+  num iof = 0.00;
+  num iofa = 0.00;
+  num totalP = 0.00;
+  double emp = 0.00;
+  double tx = 0.00;
+  DateTime date = DateTime.now();
+  num tir = 0;
+  num tarifa = 0;
+
+
+}
