cb3060cfa507d28a797e6ecfd55356e2be5fb8b3
Vitor <vmendesneto@gmail.com>
2022-07-17 02:40:03 -0300

    changes finish v.4.0.0.

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,11 @@ import 'package:calculadora_capital/src/controller/state_view.dart';
 import 'package:calculadora_capital/src/providers/theme_provider.dart';
 import 'package:calculadora_capital/src/theme/theme_color.dart';
 import 'package:calculadora_capital/views/home.dart';
+
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:splash_screen_view/SplashScreenView.dart';
 
 late SharedPreferences? prefs;
 
@@ -23,7 +25,6 @@ class MyApp extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final _width = MediaQuery.of(context).size.width;
     variables.width = _width;
-    print(_width);
     final themesNotifier = ref.read(themeProvider.notifier);
     themesNotifier.setTheme(themes[prefs!.getInt("theme") ?? 0]);
 
@@ -31,10 +32,21 @@ class MyApp extends ConsumerWidget {
       theme: themesNotifier.getTheme(),
       title: 'Simulador Bancário',
       debugShowCheckedModeBanner: false,
-      routes: {
-        '/home': (context) => const HomePage(),
-      },
-      home: const HomePage(),
+      home: SplashScreenView(
+        navigateRoute: const HomePage(),
+        duration: 5000,
+        text: "Loading.....",
+        textType: TextType.ColorizeAnimationText,
+        textStyle: const TextStyle(
+          fontSize: 40.0,
+        ),
+        colors: const [
+          Colors.white,
+          Color(0xff082567),
+          Color(0xff0000CD),
+        ],
+        backgroundColor: const Color(0xff082555),
+      ),
     );
   }
 }
